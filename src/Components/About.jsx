981196ff7cbb9942da1d8890e512adc684112b98
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-06-23 10:20:24 +0100

    update: Added texts to the about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='flex flex-col mt-8'>
+      <h1 className='text-5xl font-bold dark:text-purple-200 tracking-wide'>
+        About Me
+      </h1>
+      <div className='mt-8 dark:text-purple-100 tracking-wide flex flex-col gap-3'>
+        <p>
+          Greetings, my name is Emmanuel Amoo, a self taught and result driven developer skilled in creating and implementing eye catching and user friendly web applications.
+          </p>
+          <p>
+          As a Distinction gradute in Computer Engineering from Federal Polytechnic Ede,Osun State, I've been exposed to the diversities in tech space and have my knowledge expanded, especailly as far as the tech space is concerned.
+          With my relentless passion towards problem-solving and making things work, I have been able to stay updated with the latest trends in the web technology space.
+          </p>
+          <p>
+          I am current seeking for a fast paced environment where I can deliver at a high quality my skill set and up my game, and the same where hardwork is handsomely rewarded.  
+        </p>
+      </div>
+    </div>
   )
 }
 
