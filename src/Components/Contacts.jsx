b44fb409e3d68241fa27e3e600da1d4f682f2e3e
Emmanuel Ayodeji Amoo <hahyordeji@gmail.com>
2023-11-17 10:53:33 +0100

    Updated the twitter profile link in Contacts.jsx
    Updated the twitter profile link in Contacts.jsx

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -24,7 +24,7 @@ const Contacts = () => {
           <i className="fa-brands fa-github"></i>
         </Link>
         <Link
-          to="https://twitter.com/EmmaAyo_A"
+          to="https://twitter.com/emmycodes_"
           target="_blank"
           className="flex justify-between px-4 md:px-10 py-2 rounded-md bg-gradient-to-r from-purple-700 to-purple-400 dark:from-slate-600 dark:to-slate-400 dark:bg-slate-600 text-purple-100 font-edu tracking-widest md:text-xl font-semibold w-[80%] md:w-[50%]"
         >
